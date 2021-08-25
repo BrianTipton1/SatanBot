c7c75d9db96dd4eaee10c25ff737fa4cedef0455
Brian Tipton <Brian.p.tipton@gmail.com>
2021-08-25 18:37:48 -0500

    should fix if command "fails"

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,3 +4,8 @@ set -e
 cp /home/ubuntu/.env /home/ubuntu/SatanBot/.env
 cd /home/ubuntu/SatanBot
 docker-compose up -d --build
+function clean(){
+docker rmi $(docker images -f dangling=true -q)
+return 0
+}
+clean
