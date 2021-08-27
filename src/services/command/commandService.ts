ccc0bb6df1ab441e38b99415e0ad4db77011afa7
Brian Tipton <Brian.p.tipton@gmail.com>
2021-08-27 05:05:43 +0000

    switch to desktop

--- src/services/command/commandService.ts
+++ src/services/command/commandService.ts
@@ -1,4 +1,4 @@
-import { Message, StartThreadOptions } from 'discord.js';
+import { Message, ThreadChannel } from 'discord.js';
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../../types';
 import { TierListService } from '../tierlist/tierlistService';
@@ -10,13 +10,19 @@ export class CommandService {
         this.tierListService = tierListService;
     }
     public async handleCommand(message: Message) {
+        if (message.channel.isThread()) {
+            this.handleThreadCommand(message);
+        }
         if (message.content.includes('tierlist')) {
             this.tierListService.startTierList(message);
         }
-    }
-    private commandType(message: Message) {
-        if (message.content.includes('tierlist')) {
-            this.tierListService.startTierList(message);
+        if (message.content.includes('help')) {
+            message.reply(
+                'Satan bot currently only has one command currently\n\n' +
+                    '"-tierlist" --> Create a tierlist either an "S tier list" or a numbered list\n' +
+                    'Example tierlist command "-tierlist -alpha Hamburgers" or "-tierlist -num Hamburgers"',
+            );
         }
     }
+    private async handleThreadCommand(message: Message) {}
 }
